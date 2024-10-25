a27bef95203b7d814e22cfade3d114ba1abe96f7
Roosa Vuorela <roosavuo@gmail.com>
2024-10-25 22:42:14 +0300

    lisäys: ikonit navigaatioon

--- App.js
+++ App.js
@@ -10,14 +10,30 @@ import Ionicons from '@expo/vector-icons/Ionicons';
 const Tab = createBottomTabNavigator();
 
 export default function App() {
+  
   return (
-    <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="Etusivu" component={EtusivuScreen} />
-        <Tab.Screen name="Vaatelista" component={ListaScreen} />
-        <Tab.Screen name="Lisää vaate" component={LomakeScreen} />
-      </Tab.Navigator>
-    </NavigationContainer>
+        <NavigationContainer>
+          <Tab.Navigator
+            screenOptions={({route}) => ({
+              tabBarIcon: ({ focused, color, size}) => {
+                let iconName;
+
+                if(route.name === 'Etusivu') {
+                  iconName = 'home';
+                } else if (route.name === 'Vaatelista') {
+                  iconName = 'list';
+                } else if (route.name === 'Lisää vaate'){
+                  iconName = 'add-circle';
+                }
+                return <Ionicons name={iconName} size={size} color={color} />
+              },
+              
+            })}>
+            <Tab.Screen name="Etusivu" component={EtusivuScreen} />
+            <Tab.Screen name="Vaatelista" component={ListaScreen} />
+            <Tab.Screen name="Lisää vaate" component={LomakeScreen} />
+          </Tab.Navigator>
+        </NavigationContainer>
   );
 }
 
