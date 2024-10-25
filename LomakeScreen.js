3f5ec0815efcda9c220030b0a49cca5cfa017ef1
Roosa Vuorela <roosavuo@gmail.com>
2024-10-25 23:01:09 +0300

    lisäys: react native paper asennus, lomakkeen aloitus textinput ja button componenteilla

--- LomakeScreen.js
+++ LomakeScreen.js
@@ -1,10 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
+import { Button, TextInput } from 'react-native-paper';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function LomakeScreen() {
   return (
     <View style={styles.container}>
       <Text>Tänne lomake</Text>
+      <TextInput 
+      style={{ width: '90%', marginBottom: 10}}
+        label="Vaate"
+      />
+      <Button>Lisää listalle</Button>
       <StatusBar style="auto" />
     </View>
   );
